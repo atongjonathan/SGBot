a53d1d414bf08b2fc3b7a491b19bdf35988011b2
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-03-06 14:31:15 +0300

    feat: func startbot

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from tgbot.filters.admin_filter import AdminFilter
 from tgbot.handlers.user import UserHandler
 from tgbot.handlers.callback_handler import CallbackHandler
 
-# utils
-from tgbot.utils.database import Database
-
 # telebot
 from telebot import TeleBot, logging
 # middleware
@@ -23,53 +20,63 @@ from tgbot.config import TOKEN
 bot = TeleBot(TOKEN, parse_mode="markdown", use_class_middlewares=True)
 
 
-logging.basicConfig(format="SGBot | %(asctime)s | %(levelname)s | %(module)s | %(lineno)s | %(message)s | ", level=logging.INFO, handlers={
+logging.basicConfig(format="SGBot | %(asctime)s | %(levelname)s | %(module)s | %(lineno)s | %(message)s | ", level=logging.INFO, handlers=[
     logging.StreamHandler(),
     logging.FileHandler("logs.txt")
-})
+])
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 
-db = Database()
-user = UserHandler()
-handler  = CallbackHandler(bot)
 # async_bot = AsyncTeleBot(config.TOKEN, parse_mode="markdown")
 
 link_regex = "(https?:\\/\\/)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)?"
 
 
-def register_handlers():
+def register_handlers(user: UserHandler):
     logger.info("Registering handlers ...")
-    bot.register_message_handler(user.admin_user, commands=['start'], admin=True, pass_bot=True)
+    bot.register_message_handler(user.admin_user, commands=[
+                                 'start'], admin=True, pass_bot=True)
     bot.register_message_handler(user.start, commands=['start'], pass_bot=True)
-    bot.register_message_handler(user.command, commands=['help'], pass_bot=True)
-    bot.register_message_handler(user.logs, commands=['logs'],admin=True, pass_bot=True)
+    bot.register_message_handler(user.command, commands=[
+                                 'help'], pass_bot=True)
+    bot.register_message_handler(
+        user.logs, commands=['logs'], admin=True, pass_bot=True)
     bot.register_message_handler(user.ping, commands=['ping'], pass_bot=True)
-    bot.register_message_handler(user.artist, commands=["artist"], pass_bot=True)
+    bot.register_message_handler(user.artist, commands=[
+                                 "artist"], pass_bot=True)
     bot.register_message_handler(user.song, commands=["song"], pass_bot=True)
-    bot.register_message_handler(user.snippet, commands=["snippet"], pass_bot=True)
-    bot.register_message_handler(user.snippets, commands=["snippets"], pass_bot=True)
+    bot.register_message_handler(user.snippet, commands=[
+                                 "snippet"], pass_bot=True)
+    bot.register_message_handler(user.snippets, commands=[
+                                 "snippets"], pass_bot=True)
     bot.register_message_handler(user.regex, regexp=link_regex, pass_bot=True)
-    bot.register_message_handler(user.admin_trending, commands=["trending"],admin=True, pass_bot=True)
-    bot.register_message_handler(user.trending, commands=["trending"],admin=False, pass_bot=True)
+    bot.register_message_handler(user.admin_trending, commands=[
+                                 "trending"], admin=True, pass_bot=True)
+    bot.register_message_handler(user.trending, commands=[
+                                 "trending"], admin=False, pass_bot=True)
     bot.register_message_handler(user.handle_text, pass_bot=True)
     logger.info("Finished registering handlers successfully!")
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
+    handler = CallbackHandler(bot)
     handler.process_callback_query(call, bot)
 
 
-# Middlewares
-
-if __name__ == "__main__":
+def start_bot():
+    user = UserHandler()
     bot.setup_middleware(AntiFloodMiddleware(limit=2, bot=bot))
     bot.setup_middleware(QueryMiddleware(bot=bot))
 
-# custom filters
+    # custom filters
     bot.add_custom_filter(AdminFilter())
-    register_handlers()
-    logger.info("Bot is running ... ")
+    register_handlers(user)
+    logger.info("____Bot is running___")
     bot.polling()
+
+# Middlewares
+
+
+if __name__ == "__main__":
+    start_bot()
