a20e11b8eb7e7cd73554c5e1115c971ba5045c6a
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-04-27 20:28:30 +0300

    Revert "fix: synced lyrics"
    This reverts commit 9d6d46c3cd240ef75a738d9739b29868f956c324.

--- tgbot/utils/lyrics.py
+++ tgbot/utils/lyrics.py
@@ -1,6 +1,6 @@
 from tgbot.config import MUSICXMATCH_API_KEY
 from logging import getLogger
-from spotdl.providers.lyrics import MusixMatch, Genius, Synced
+from spotdl.providers.lyrics import MusixMatch, Genius
 import requests
 
 
@@ -8,7 +8,7 @@ class Lyrics():
 
     def __init__(self) -> None:
         self.musixmatch = MusixMatch()
-        self.genius = Synced()
+        self.genius = Genius()
         self.logger = getLogger(__name__)
 
     def musicxmatch_lyrics(self, artist, title):
@@ -31,9 +31,6 @@ class Lyrics():
         return lyrics
 
     def get_lyrics(self, artist, title):
-      genius_l = self.genius_lyrics(artist, title)
-      if genius_l is None:
+        if self.genius_lyrics is None:
             return self.musicxmatch_lyrics(artist, title)
-      else:
-        return genius_l
 
