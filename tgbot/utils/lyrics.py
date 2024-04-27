4c217ce6a2f950e14eaa77fda1360cad787762b3
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-04-27 05:54:34 +0300

    feat: synced kyrics

--- tgbot/utils/lyrics.py
+++ tgbot/utils/lyrics.py
@@ -1,6 +1,6 @@
 from tgbot.config import MUSICXMATCH_API_KEY
 from logging import getLogger
-from spotdl.providers.lyrics import MusixMatch, Genius
+from spotdl.providers.lyrics import MusixMatch, Genius, Synced
 import requests
 
 
@@ -8,7 +8,7 @@ class Lyrics():
 
     def __init__(self) -> None:
         self.musixmatch = MusixMatch()
-        self.genius = Genius()
+        self.genius = Synced()
         self.logger = getLogger(__name__)
 
     def musicxmatch_lyrics(self, artist, title):
@@ -22,6 +22,7 @@ class Lyrics():
         track_data = response.json()
         long_link = track_data["message"]["body"]["track_list"][0]["track"]["track_share_url"]
         link = long_link.split("?")[0]
+        print(link)
         song_lyrics = self.musixmatch.extract_lyrics(link)
         return song_lyrics
 
@@ -31,6 +32,9 @@ class Lyrics():
         return lyrics
 
     def get_lyrics(self, artist, title):
-        if self.genius_lyrics is None:
+      genius_l = self.genius_lyrics(artist, title)
+      if genius_l is None:
             return self.musicxmatch_lyrics(artist, title)
+      else:
+        return genius_l
 
