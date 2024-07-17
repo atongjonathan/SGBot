fa9ec11178c2e594c1f0ac62d037b5356f695e54
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-07-17 17:05:17 +0300

    fix: return init codes

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,1 +1,12 @@
 
+from telebot import TeleBot
+from tgbot.config import TOKEN
+from .callback_handler import CallbackHandler
+from .artist_handler import ArtistHandler
+from .song_handler import SongHandler
+from .user import UserHandler
+
+bot = TeleBot(TOKEN, parse_mode="markdown")
+callbackhandler = CallbackHandler(bot)
+artisthandler = ArtistHandler(bot)
+songhandler = SongHandler(bot)
