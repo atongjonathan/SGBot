cf4c88eebb94996863b37c89e424c237cb43cb4b
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-08-16 09:04:05 +0300

    fix: stringify error

--- tgbot/utils/flask_api.py
+++ tgbot/utils/flask_api.py
@@ -8,6 +8,8 @@ from .functions import download
 from tgbot.utils.database import Database
 from tgbot.utils.spotify import Spotify
 from tgbot.handlers.song_handler import SongHandler
+
+
 load_dotenv("config.env")
 users_json = os.environ.get("USERS")
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
@@ -18,6 +20,8 @@ bot = telebot.TeleBot(TOKEN)
 song_handler = SongHandler(bot)
 spotify = Spotify()
 database = Database()
+
+
 try:
     if users_json:
         users = json.loads(users_json)
@@ -45,11 +49,12 @@ def start():
 @auth.login_required
 def callback():
     kwargs = request.json
+    telebot.logging.info(f"Callback received: {kwargs}")
     kwargs["chat_id"] = DB_CHANNEL
     track_url = kwargs["track_url"]
-    track_details = spotify.get_chosen_song(track_url) 
+    track_details = spotify.get_chosen_song(track_url)
     kwargs["performer"] = ", ".join(track_details["artists"])
-    kwargs["title"] = track_details["name"]  
+    kwargs["title"] = track_details["name"]
     kwargs["reply_markup"] = kwargs["hashtag"] = ""
     is_download_successfull = download(track_link=kwargs["track_url"],
                                        cwd=str(kwargs["chat_id"]))
@@ -68,8 +73,7 @@ def callback():
                 TOKEN, file_info.file_path)
             return jsonify({"url": url}), 200
         except Exception as e:
-            return jsonify({"error": e, "url": None}), 500
-    # return jsonify({"error": "Download Unsuccessful!"}), 500
+            return jsonify({"error": str(e), "url": None}), 500
 
 
 def run_api():
